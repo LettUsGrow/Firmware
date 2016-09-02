c032dc1de76c50f0f1a896b0895c762d49f0c6c8
BenLettUsGrow <ben@lettusgrow.org>
2016-09-02 15:12:37 +0100

    Add files via upload

---
+++ LED_Control_Code.ino
@@ -0,0 +1,42 @@
+/*
+LED Light Intensity Control
+
+
+ The analogWrite() function uses PWM, so if
+ you want to change the pin you're using, be
+ sure to use another PWM capable pin. On most
+ Arduino, the PWM pins are identified with 
+ a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
+
+ */
+
+int led1 = 9; // the PWM pin the LED is attached to
+int led2 = 10;
+int brightness = 100;   // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // declare pin 9 and 10 to be an output:
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // set the brightness of pin 9:
+  analogWrite(led1, brightness);
+  analogWrite(led2, brightness);
+
+  // change the brightness for next time through the loop:
+   // brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount ;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(30);
+  // wait for 3 seconds between phases
+  //delay(3000);
+}
---
+++ Server_Test.ino
@@ -0,0 +1,66 @@
+*
+ * Test server for Lettus Grow.
+ * 
+ * Basic idea is that the Node server sends simple commands to this
+ * server as trailing slashes after this servers address (crudely).
+ * Only works if the Node server is on the same Wifi network.
+ * Currently only works with an Arduino Uno Wifi.
+ *
+ * Created 30th June 2016
+ * By Louis JA
+ * Modified Last Time 4th July by Reinaldo Melo
+ */
+
+
+#include <Wire.h>
+#include <ArduinoWiFi.h>
+
+void setup() {
+  Serial.begin(9600);
+  Wifi.begin();                                     // Start Wifi server
+}
+void loop() {
+  while (Wifi.available()) {
+    process(Wifi);
+  }
+}
+
+void process(WifiData client) {
+  String command = client.readStringUntil('/');    // Read the command
+  
+  String response = "HTTP/1.1 200 OK\n";           // HTTP header
+  response += "Content-Type: application/json\n";
+  response += "Accept: application/json\n";
+  response += "Connection: close\n\n";             // Don't keep the connection open after sending this data
+
+  if(command == "updateLight") {                         // Can't switch on String's in Arduino's Java
+    response += lights(client);
+  } else {
+    response += invalid();
+  }
+
+  response += DELIMITER;                           // Signals the end of the HTTP request
+  client.print(response);                          // Send to client
+}
+
+// Update the value of a particular light
+String updateLight() {
+  if (client.read() != '/') {                      // If the next character is not a '/' we have a malformed URL
+    return invalid();
+  }
+  
+  int light = client.parseInt();                   // Which light are we changing
+  
+  if (client.read() != '/') { 
+    return invalid(); 
+  }
+  
+  int value = client.parseInt();
+  
+  /* INSERT CODE HERE */
+  return "";
+}
+
+String invalid() {
+  return "{ \"error\": 0 }\n";
+}https://codeshare.io/6arE5
