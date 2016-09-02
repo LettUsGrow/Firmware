19902e479976fed04a8e6ebd2ee897ef1de221a8
BenLettUsGrow <ben@lettusgrow.org>
2016-09-02 21:19:24 +0100

    Update LED_Control_Code.ino
    Setup to react correctly with potentiometer.
    Flickers a lot when deciding to come on or not, i.e. around the 100/99% ambient light values. I would suggest implementing some delayed on or off, rather than fast response. Looks weird
    Also intermittent flashing when left resting at 100% due to flickering pot values. Similar solution to above.
    
    LED gives really sharp on or off point.
    
    Next to add is a way of setting clock without server connection and then setting LED on and off times.

--- LED_Control_Code.ino
+++ LED_Control_Code.ino
@@ -1,13 +1,10 @@
 /*
 LED Light Intensity Control
-
-
  The analogWrite() function uses PWM, so if
  you want to change the pin you're using, be
  sure to use another PWM capable pin. On most
  Arduino, the PWM pins are identified with 
  a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
  */
 
 int led1 = 5; // the PWM pin the LED is attached to
@@ -15,52 +12,68 @@ int led2 = 6;
 int led3 = 9;
 int led4 = 10;
 int fogger = 0; // set fogger output pin no. (not PWM)
-// int ambientlight = A0; //set light sensor input pin
+int ambientlight = A0; //set light sensor input pin
 
 
 // SET DEFAULT VARIABLES
 int defaultbrightness = 100;   // Default LED desired brightness %age
-int fadeAmount = 5;    // LED intensity change steps      %age
-int ledon = 0700;          //set default LED on time       
-int ledoff = 0800;         //set default LED off time
+int fadeamount = 1;    // LED intensity change steps      %age
+int ledon ;          //set default LED on time       
+int ledoff;         //set default LED off time
 int fogon = ledon;         //set fogger on time
 int fogoff = ledoff;       //set fogger off time
 int foghumidity = 100;    // set default desired fog humidity %age
 int startbrightness = 0;
+int targetbrightness;
+int ledbrightness;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare led pins to be an output:
   pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(fogger, OUTPUT);
+  //pinMode(led2, OUTPUT);
+  //pinMode(led3, OUTPUT);
+  //pinMode(led4, OUTPUT);
+  //pinMode(fogger, OUTPUT);
   //declare input pins
   pinMode(A0, INPUT);
   // set variables
-  int targetbrightness = defaultbrightness;
-  int ledbrightness = startbrightness;
+  targetbrightness = defaultbrightness;
+  ledbrightness = startbrightness;
   analogWrite(led1, ledbrightness);
-  
+
+  //setup serial output for Pot (testing only)
+  Serial.begin(9600);
+    
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
  // set local clock
  // should the lights be on?
- //if(hour() >= hour(ledon){
- 
+ //if(hour() >= hour(ledon)){
 
-  int ambientbrightness = 100*A0/1023;  //read ambient light sensor %age
-  
-  if(ambientbrightness<targetbrightness){    //if the ambient brightness is lower than the target brightness
+
+ 
+  int ambientbrightness = analogRead(A0)/10.23;  //read ambient light sensor %age
+ Serial.print(ambientbrightness);
+ Serial.print("    ");
+ Serial.print(ledbrightness);
+   Serial.print("    ");
+ Serial.println(ledbrightness*2.56);
+ 
+  if(ambientbrightness < targetbrightness){    //if the ambient brightness is lower than the target brightness
    int ledtargetbrightness = targetbrightness-ambientbrightness;   //calculate the additional LED brighness to top upto perfect
    
-   if (ledbrightness != ledtargetbrightness){ //if the current led brightness is not correct the change it
-    ledbrightness = ledbrightness + sign(ledtargetbrightness);  //increase or decrease the brightness by the step amount
-    analogWrite(led1, ledbrightness); //output the new value
+   if (ledbrightness < ledtargetbrightness){ //if the current led brightness is not correct the change it
+    ledbrightness = ledbrightness + fadeamount;  //increase or decrease the brightness by the step amount
+    analogWrite(led1, ledbrightness*2.56-2.56); //output the new value
    }
+   if (ledbrightness > ledtargetbrightness){ //if the current led brightness is not correct the change it
+    ledbrightness = ledbrightness - fadeamount;  //increase or decrease the brightness by the step amount
+    analogWrite(led1, ledbrightness*2.56-2.56); //output the new value
+   }
+   delay(25);
   }
  //}
 }
@@ -81,6 +94,6 @@ void ServerSet(){  //ping the server to set variables
   //set fogger humidty from server
 }
 
-void ServerData{  //send data to the server
-}
+void ServerData(){  //send data to the server
+
 }
