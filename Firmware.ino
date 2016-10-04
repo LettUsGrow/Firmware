33953f7c7ddba6f724e8213f4ef51246f2faafc8
BenLettUsGrow <ben@lettusgrow.org>
2016-10-04 09:15:53 +0100

    Update Firmware.ino

--- Firmware.ino
+++ Firmware.ino
@@ -7,27 +7,37 @@ LED Light Intensity Control
  a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
  */
 
+#include <SimpleDHT.h>
 
-// include libraries
+SimpleDHT11 DHT11;
 
-#include<Time.h>
-#include <TimeAlarms.h> // seems to be needed to use setTime command
+#include <TimeLib.h>
 
-//Set pin variables
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
 
-int ledredpin = 5;              //set red led pin
-int ledbluepin = 6;             // set blue led pin
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <WidgetRTC.h>
+
+
+
+  //  Set global variables
+int ledredpin = D1;              //set red led pin
+int ledbluepin = D2;             // set blue led pin
 int lightsensorpin = A0;        //set light sensor pin
 
-int foggerpin = 4;              //set fogger output pin
-int humiditysensorpin = A1;     // set humidty sensor pin
+int foggerpin = D4;              //set fogger output pin
+int humiditysensorpin = D8;     // set humidty sensor pin
 
-int watersensorpin = A2;        // set water sensor pin
+int watersensorpin = A0;        // set water sensor in
+int lightsensorswitch = D5;
+int watersensorswitch = D6;
 
 // Set defalt operating values
 
 int targetbrightness = 80;     // target light intensity as %age
-int lightpgain = 1;             // proportional gain for light control code CURRENTLY UNUSED
 
 int targethumidity = 50;        // target humidty as %age
 int humiditylow = 25;
@@ -35,88 +45,180 @@ int humiditylow = 25;
 int warningwaterlevel = 50;     // water level %age to display warning
 int stopwaterlevel = 25;        // water level %age to terminate fogger
 
-//LED and fog starting values
-int ledon = 0;
-int fog = 0;
+// create operating values
+int ambientbrightness = 13;
+int waterlevel = 47;
+byte humidity = 15;
+byte temperature = 15;
 
-// set fogger start and end hour
-int foggeronhour = 8;
-int foggeroffhour = 20;
 
 // set light start and end hour
-int lightonhour = 8;
-int lightoffhour = 20;
+int onhour = 8;
+int onminute = 0;
+int offhour = 20;
+int offminute = 0;
 
 //set starting light brightness
 int ledbrightness = 0;
-float redbalance = 0.6;
-int red;
-int blue;
+float redbalance = 0.5;
+float red;
+float blue;
 
-// water level warnings
+BLYNK_WRITE(V1){
+  targetbrightness = param.asInt();
+}
+BLYNK_WRITE(V2){
+  redbalance = param.asFloat()/100;
+}
+BLYNK_WRITE(V3){
+  targethumidity = param.asInt();
+}
+BLYNK_READ(V4){
+   Blynk.virtualWrite(V4, waterlevel);
+}
+BLYNK_WRITE(V6){
+  TimeInputParam t(param);
+  if (t.hasStartTime()){
+    onhour = t.getStartHour();
+    onminute = t.getStartMinute();
+  }
+  if (t.hasStopTime()){
+    offhour = t.getStopHour();
+    offminute = t.getStopMinute();
+  }
+}
 
-String warning;
+WidgetRTC rtc;
+BLYNK_ATTACH_WIDGET(rtc,V5);
+
+
+char auth[] = "15721cd4ce454bc99a15c7073f5e9ac9";
+
+char ssid[] = "Triangle";
+char pass[] = "CliftonLife6";
+
+WiFiClient client;
+
+unsigned long myChannelNumber = 162238;
+const char * myWriteAPIKey = "XXBMA2W43TTEQEHP";
+
 
 
 void setup() {
-
+  Serial.begin(115200);
+  Serial.println("Set pins");
+  
   //set output pins
   pinMode(ledredpin, OUTPUT);
   pinMode(ledbluepin, OUTPUT);
   pinMode(foggerpin, OUTPUT);
+  pinMode(lightsensorswitch, OUTPUT);
+  pinMode(watersensorswitch, OUTPUT);
 
   //set input pins
   pinMode(lightsensorpin, INPUT);
   pinMode(humiditysensorpin, INPUT);
   pinMode(watersensorpin, INPUT);
 
-  //set local clock
-  setTime(7, 59, 50, 1, 1, 2016);
+  Serial.println("Set time");
+  
+  setTime(7,59,30,1,1,2015);
 
-  // start serial output
-  Serial.begin(9600);
+  Serial.println("Set wifi");
+  WiFi.begin(ssid,pass);
 
-  // set variables
-  ledbrightness = 0;
+  Serial.println("Start Thing Speak");
+  
+  ThingSpeak.begin(client);
 
+  Serial.println("Start Blynk");
+  
+  Blynk.begin(auth, ssid, pass);
+
+  rtc.begin();
+
+  Blynk.syncAll();
+
+  Serial.println("Setup Complete");
 }
 
 void loop() {
-  int ambientbrightness = 100 - (analogRead(lightsensorpin) / 10.23);   // reads the input from the light sensor and normalises to %age of the max 1023 output
-  int waterlevel = analogRead(watersensorpin) / 10.23;                  // reads the water level sensor and converts to %age
-  int humidity = analogRead(humiditysensorpin) / 10.23;                 // reads humidity sensor as a %age
+  //Read sensors
+  digitalWrite(lightsensorswitch, HIGH);
+  delay(10);
+  ambientbrightness = analogRead(lightsensorpin)/10.23;
+  delay(10);
+  digitalWrite(lightsensorswitch, LOW);
 
-  if ( waterlevel <= 50 && waterlevel > 25 ) {
-    warning = "Water low";
+  delay(10);
+
+  digitalWrite(watersensorswitch, HIGH);
+  delay(10);
+  waterlevel = analogRead(watersensorpin) / 10.23;                  // reads the water level sensor and converts to %age
+  delay(10);
+  digitalWrite(watersensorswitch, LOW);
+  
+//Serial.println("read hum temp");
+  DHT11.read(humiditysensorpin, &temperature, &humidity, NULL);
+
+  //run blynk
+  Blynk.run();
+  
+  //IF wifi, thingspeak, blynk not connected, restart each of them
+
+  if (second() == 0){
+    WiFi.begin(ssid,pass);
+    if(Blynk.connect() == false){
+      Serial.print("Blynk Disconnected - Reconnecting");
+      Blynk.begin(auth, ssid, pass);
+      delay(500);
+    }
+    
+    Serial.println("upload data");
+    Blynk.syncAll();
+    
+    ThingSpeak.setField(1, ambientbrightness);
+    ThingSpeak.setField(2, waterlevel);
+    ThingSpeak.setField(3, humidity);
+    ThingSpeak.setField(4, temperature);
+    ThingSpeak.setField(5, targetbrightness);
+    ThingSpeak.setField(6, redbalance);
+    ThingSpeak.setField(7, targethumidity);
+    ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);    
+    delay(1000);
   }
-  else if ( waterlevel <= 25 ) {
-    warning = "No Water";
+
+//Water level change blynk colours SLOWS CLOCK SPEED MASSIVELY!
+/*Serial.println("run water logic");
+  if ( waterlevel <= warningwaterlevel && waterlevel > stopwaterlevel ) { //running a bit low
+    Blynk.setProperty(V4, "color", "#ED9D00");
+  }
+  else if ( waterlevel <= stopwaterlevel ) {
+    Blynk.notify(String("Please refill the water"));
+    Blynk.setProperty(V4, "color", "#D3435C");
   }
   else {
-    warning = "Water ok";
+    Blynk.setProperty(V4, "color", "#04C0F8");
   }
-
- 
-  if ( (hour() >= foggeronhour) && (hour() < foggeroffhour) && (waterlevel >= 25)){  // checks if the fogger should be on
+*/
+//Serial.println("run fog logic");
+  
+  if ((hour() >= onhour) && (hour() < offhour) && (minute() >= onminute) && (minute() < offminute) && (waterlevel >= 25)){  // checks if the fogger should be on
     if (humidity < humiditylow){
-      fog = 1; //placeholder for now
       digitalWrite(foggerpin, HIGH);                              // signal to turn on fogger relay
     }
     else if( humidity >= targethumidity) {
-      fog = 0;
       digitalWrite(foggerpin, LOW);
     }
   }
-  
-  if ((hour() >= lightonhour) && (hour() < lightoffhour)){     // checks the lights should be on
+//Serial.println("run light logic");
+    if ((hour() >= onhour) && (hour() < offhour) && (minute() >= onminute) && (minute() < offminute)){     // checks the lights should be on
     if( ambientbrightness < targetbrightness ){                // check the light needs topping up
-      ledbrightness = 0.25* (targetbrightness - ambientbrightness -1) ;  //-1 ensures the LED doesnt light up at zero output
+      ledbrightness = (targetbrightness - ambientbrightness -1) ;  //-1 ensures the LED doesnt light up at zero output
 
       red = redbalance*ledbrightness*2.55;
       blue = (1-redbalance)*(ledbrightness*2.55);
-
-
-      
+     
       analogWrite(ledredpin, red);        // output to red led
       analogWrite(ledbluepin, blue);   //output to blue led
     }
@@ -130,26 +232,35 @@ void loop() {
     analogWrite(ledredpin, 0);
     analogWrite(ledbluepin, 0);
   }
+
+  Serial.print(year());
+  Serial.print("  ");
   Serial.print(hour());
   Serial.print(":");
   Serial.print(minute());
   Serial.print(":");
   Serial.print(second());
-  Serial.print("  Ambient: ");
+  Serial.print("  ");
   Serial.print(ambientbrightness);
-  Serial.print("   LED: ");
-  Serial.print("Red: ");
+  Serial.print("  ");
   Serial.print(red);
-  Serial.print("  Blue: ");
+  Serial.print("  ");
   Serial.print(blue);
-  Serial.print("   All: ");
-  Serial.print(ledbrightness);
-  Serial.print("   Water level: ");
+  Serial.print("  ");
+  Serial.print(redbalance);
+  Serial.print("  temp: ");
+  Serial.print(temperature);
+  Serial.print("  hum: ");
+  Serial.print(targethumidity);
+  Serial.print("  wat: ");
   Serial.print(waterlevel);
-  Serial.print("   Humidity: ");
-  Serial.print(humidity);
-  Serial.print("   Fogger: ");
-  Serial.print(fog);
-  Serial.print("   ");
-  Serial.println(warning);
+  Serial.print("  Start: ");
+  Serial.print(onhour);
+  Serial.print(":");
+  Serial.print(onminute);
+  Serial.print("  Stop: ");
+  Serial.print(offhour);
+  Serial.print(":");
+  Serial.println(offminute);
+  
 }
